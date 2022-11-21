6f602f327e938b0fddb49a6c52d4cd43ebce566b
zhili <2760873644@qq.com>
2022-11-22 02:12:59 +0800

    Squashed commit of the following:
    commit 0ac26ead6e8143a67f051f85456a0dee96183eb3
    Author: Master <2363959328@qq.com>
    Date:   Sat Nov 19 14:30:48 2022 +0800
    
        架构更改2次
    
    commit 5b34ce865d2f82b6b62d085a288030662324d2fd
    Author: Master <2363959328@qq.com>
    Date:   Sat Nov 19 09:59:34 2022 +0800
    
        提交项目架构
    
    commit b5a75e5929e641023becccc22aaff832737b4e28
    Author: Master <2363959328@qq.com>
    Date:   Sat Nov 19 00:00:43 2022 +0800
    
        项目架构更改1次
    
    commit 082cd2833dbcf8d6e760afb169b57fe86acecca4
    Author: Master <2363959328@qq.com>
    Date:   Fri Nov 18 22:24:58 2022 +0800
    
        提交项目架构

--- zhili's_branch.cpp
+++ zhili's_branch.cpp
@@ -1,8 +1,104 @@
-#include<iostream>
-
+#include <graphics.h> 
+#include <conio.h>
+#include<stdio.h>
+#include<easyx.h>
+#include "Interface_Bace.h"
 using namespace std;
 
-int main()
+void button(int x, int y, int w, int h, TCHAR* text)//按钮设置函数
 {
-	cout << "test test" << endl;
+    setbkmode(TRANSPARENT);
+    setfillcolor(GREEN);
+    fillroundrect(x, y, x + w, y + h, 10, 10);
+    // 输出字符串（MBCS 字符集）
+    TCHAR s1[] = L"黑体";
+    settextstyle(30, 0, s1);
+    TCHAR s[50] = L"hello";
+
+    int tx = x + (w - textwidth(text)) / 2;
+    int ty = y + (h - textheight(text)) / 2;
+
+    outtextxy(tx, ty, text);
+
+}
+void Interface_Bace::opening_menu()
+{
+    initgraph(640, 480);    // 创建绘图窗口，大小为 640x480 像素
+    IMAGE back_img;
+    loadimage(&back_img, _T("文件路径"), 640, 480, true);
+    putimage(0, 0, &back_img);
+
+    TCHAR s1[50] = L"娱乐模块";
+    TCHAR s2[50] = L"学习模块";
+    TCHAR s3[50] = L"返回";
+
+    button(440, 150, 170, 50, s1);
+    button(440, 250, 170, 50, s2);
+    button(440, 350, 170, 50, s3);
+
+    //鼠标操作
+    ExMessage msg;
+    while (true) {
+        if (peekmessage(&msg, EM_MOUSE)) {
+
+            switch (msg.message)
+            {
+            case WM_LBUTTONDOWN:
+                if (msg.x >= 440 && msg.x <= 440 + 170 && msg.y >= 150 && msg.y <= 150 + 50)//数据依次为:左，左+长，上，上+宽
+                {
+                    //outtextxy(200, 200, s1);
+                    Interface_Bace::Entertainment_module();//页面跳转，说实话我不清楚怎么实现。
+                }
+                if (msg.x >= 440 && msg.x <= 440 + 170 && msg.y >= 250 && msg.y <= 250 + 50)
+                {
+                    //outtextxy(200, 200, s2);
+                    Interface_Bace::Learning_module();
+                }
+                if (msg.x >= 440 && msg.x <= 440 + 170 && msg.y >= 350 && msg.y <= 350 + 50)
+                {
+                    //outtextxy(200, 200, s3);
+                    Interface_Bace::login_interface();
+                }
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    return;
+}
+
+void Entertainment_module()
+{
+    initgraph(640, 480);    // 创建绘图窗口，大小为 640x480 像素
+    IMAGE back_img;         // 游戏页面的图片，感觉截屏应该可以
+    loadimage(&back_img, _T("文件路径"), 640, 480, true);
+    putimage(0, 0, &back_img);
+
+    //设置按钮
+    TCHAR s1[50] = L"娱乐模块";
+    TCHAR s2[50] = L"学习模块";
+
+    button(240, 450, 170, 50, s1);
+    button(440, 450, 170, 50, s2);
+
+    //鼠标操作
+    ExMessage msg;
+    while (true) {
+        if (peekmessage(&msg, EM_MOUSE)) {
+
+            switch (msg.message)
+            {
+            case WM_LBUTTONDOWN:
+                if (msg.x >= 240 && msg.x <= 240 + 170 && msg.y >= 450 && msg.y <= 450 + 50)
+                    firstgame();
+                if (msg.x >= 440 && msg.x <= 440 + 170 && msg.y >= 450 && msg.y <= 450 + 50)
+                    secondgame();
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    return;
 }
